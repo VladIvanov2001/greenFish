67536ba45ba1199037826a553e7e642a61db1ed4
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-11-11 19:38:19 +0300

    add: part of map

--- greenFish/js/yandex_map.js
+++ greenFish/js/yandex_map.js
@@ -1,5 +1,20 @@
 ymaps.ready(init);
-function init () {
+
+function init() {
+
+    var clusterIcons=[{
+        href:'./images/home-page/label.svg',
+        size:[53,52],
+        offset:[0,0]
+    }]
+
+
+
+    const coords = [
+        [53.90, 27.59],
+        [54.90, 27.56],
+        [52.90, 27.56]
+    ]
     myMap = new ymaps.Map('map', {
         center: [53.90, 27.56],
         zoom: 10
@@ -7,7 +22,29 @@ function init () {
         searchControlProvider: 'yandex#search'
     });
 
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
+
+    var myGeoObjects = [];
+
+    for (var i = 0; i < coords.length; i++) {
+        myGeoObjects[i] = new ymaps.GeoObject({
+                geometry: {
+                    type: "Point",
+                    coordinates: coords[i]
+                },
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: './images/home-page/label.svg',
+                iconImageSize: [37, 49],
+
+            },
+        );
+    }
+
+    var myClusterer = new ymaps.Clusterer({
+        clusterIcons:clusterIcons,
+    });
+    myClusterer.add(myGeoObjects);
+    myClusterer.options.set({})
+
+    myMap.geoObjects.add(myClusterer);
 }
