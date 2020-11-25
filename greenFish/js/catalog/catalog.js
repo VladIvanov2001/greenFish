0c608a2489092093fbc4c791015bd6e2c00d47b5
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2020-11-26 02:03:48 +0300

    add: select

--- greenFish/js/catalog/catalog.js
+++ greenFish/js/catalog/catalog.js
@@ -1,23 +1,53 @@
+
+for (const dropdown of document.querySelectorAll(".custom-select-wrapper")) {
+    dropdown.addEventListener('click', function () {
+        this.querySelector('.custom-select').classList.toggle('open');
+    })
+}
+
+for (const option of document.querySelectorAll(".custom-option")) {
+    option.addEventListener('click', function () {
+        if (!this.classList.contains('selected')) {
+            this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+            this.classList.add('selected');
+            this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+        }
+    })
+}
+
+// window.addEventListener('click', function (e) {
+//     const select = document.querySelector('.custom-select')
+//     if (!select.contains(e.target)) {
+//         select.classList.remove('open');
+//     }
+// });
+
+window.addEventListener('click', function (e) {
+    for (const select of document.querySelectorAll('.custom-select')) {
+        if (!select.contains(e.target)) {
+            select.classList.remove('open');
+        }
+    }
+});
+
 const paletteColors = document.querySelectorAll('.main-info__aside__palette-content-item');
 const sizes = document.querySelectorAll('.main-info__aside__size-content-item');
 
-console.log(paletteColors)
-
-paletteColors.forEach((color) =>{
-    color.addEventListener('click', () =>{
-        paletteColors.forEach((color)=>{
+paletteColors.forEach((color) => {
+    color.addEventListener('click', () => {
+        paletteColors.forEach((color) => {
             color.classList.remove('active-palette');
-        })
+        });
         color.classList.add('active-palette');
     })
-})
+});
 
-sizes.forEach((size) =>{
-    size.addEventListener('click', () =>{
-        console.log('click')
-        sizes.forEach((size)=>{
+sizes.forEach((size) => {
+    size.addEventListener('click', () => {
+        sizes.forEach((size) => {
             size.classList.remove('active-size');
-        })
+        });
         size.classList.add('active-size');
     })
-})
+});
+
