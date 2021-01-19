4b917a996f6bea3eb32b7fc0f4424576fb967f76
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2021-01-19 13:43:08 +0300

    add validation for order

--- greenFish/js/basket/basket.js
+++ greenFish/js/basket/basket.js
@@ -4,6 +4,11 @@ const itemsCounter = document.querySelectorAll('.item-count');
 const costForOneItems = document.querySelectorAll('.cost-for-one-item');
 const finalCosts = document.querySelectorAll('.final-cost');
 const sendByPost = document.getElementById('mail');
+const phoneInput = document.querySelector('.phone');
+const emailInput = document.querySelector('.email');
+const nameInput = document.querySelector('.name');
+const sendOrderBtn = document.querySelector('.basket-main__right__send-order');
+const adressOrCommentInput = document.querySelector('.class');
 let finalOrderPriceTablet = document.querySelector('.final-order-tablet__sum-numbers span');
 let finalOrderAmountTablet = document.querySelector('.final-order-tablet__count .title-small span');
 let finalOrderAmountDesktop = document.querySelector('.final-count span');
@@ -11,12 +16,51 @@ let finalOrderPriceDesktop = document.querySelector('.final-sum span');
 
 sendByPost.disabled = true;
 
-function checkAccessForMail(count){
-    if(count >= 5){
+const phoneMask = {
+    mask: '+{375}(00)0000000'
+}
+
+const mask = new IMask(phoneInput, phoneMask);
+const necessaryNumbersForPhone = 15;
+
+/*phoneInput.addEventListener('keyup', ()=>{
+   console.log(phoneInput.value.replace(/\D+/g, '').length);
+});*/
+
+function validation() {
+    let flag = true;
+    console.log(phoneInput.value.length);
+    if (nameInput.value.length === 0) {
+        flag = false;
+        nameInput.classList.add('error-input');
+    } else nameInput.classList.remove('error-input');
+    if (phoneInput.value.length !== necessaryNumbersForPhone) {
+        flag = false;
+        phoneInput.classList.add('error-input');
+    } else phoneInput.classList.remove('error-input');
+    if (!/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(emailInput.value)) {
+        flag = false;
+        emailInput.classList.add('error-input');
+    } else emailInput.classList.remove('error-input');
+    if (adressOrCommentInput.value.length === 0) {
+        flag = false;
+        nameInput.classList.add('error-input');
+    } else nameInput.classList.remove('error-input');
+    if (flag) {
+        sendOrderBtn.classList.add('send-btn-active');
+    } else sendOrderBtn.classList.remove('send-btn-active');
+}
+
+nameInput.addEventListener('keyup', validation)
+phoneInput.addEventListener('keyup', validation)
+emailInput.addEventListener('keyup', validation)
+adressOrCommentInput.addEventListener('keyup', validation)
+
+function checkAccessForMail(count) {
+    if (count >= 5) {
         sendByPost.disabled = false;
         document.querySelector('.info-about-mail').classList.add('info-about-mail-active');
-    }
-    else {
+    } else {
         sendByPost.disabled = true;
         document.querySelector('.info-about-mail').classList.remove('info-about-mail-active');
     }
@@ -24,7 +68,7 @@ function checkAccessForMail(count){
 
 function countFinalAmount() {
     let result = 0;
-    itemsCounter.forEach(elem =>{
+    itemsCounter.forEach(elem => {
         result += Number(elem.textContent);
     });
     return result;
@@ -32,7 +76,7 @@ function countFinalAmount() {
 
 function countFinalPrice() {
     let result = 0;
-    finalCosts.forEach(elem =>{
+    finalCosts.forEach(elem => {
         result += Number(elem.textContent.replace(',', '.'));
     });
     return result;
