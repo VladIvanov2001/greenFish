b249288be5d8a1418e4d2c1dc1b0934fadb1dace
VladIvanov2001 <45943204+VladIvanov2001@users.noreply.github.com>
2021-02-03 19:42:16 +0300

    add: full shop-location

--- greenFish/js/shop-location/shop-location.js
+++ greenFish/js/shop-location/shop-location.js
@@ -1,11 +1,12 @@
 const shopLocationsItems = document.querySelectorAll('.shops-location__list__item');
 const checkButtons = document.querySelectorAll('.item-checkbox');
 
-shopLocationsItems.forEach((shopLocationItem) =>{
-    shopLocationItem.addEventListener('click', ()=>{
+shopLocationsItems.forEach((shopLocationItem, idx) =>{
+    shopLocationItem.addEventListener('click', (event)=>{
         checkButtons.forEach((checkButton) =>{
-            checkButton.checked = true;
-        })
+            checkButton.checked = false;
+        });
+        checkButtons[idx].checked = true;
     })
 
 });
